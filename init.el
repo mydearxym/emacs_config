cce06653fc6a765aa3a0bfa394170224c9504082
mydearxym <mydearxym@qq.com>
2016-04-12 18:34:05 +0800

    web-mode set tab width to 2

--- init.el
+++ init.el
@@ -425,6 +425,10 @@ layers configuration."
   (setq javascript-indent-level 2)
   (setq-default indent-tabs-mode nil)
 
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+
 
   (require 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
